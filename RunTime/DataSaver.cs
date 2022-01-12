87fc225e38ee4f73498a7336394ed9d8425b7a29
suisuishou <blightue@gmail.com>
2022-01-13 00:39:15 +0800

    Test job system for writing file

--- RunTime/DataSaver.cs
+++ RunTime/DataSaver.cs
@@ -1,5 +1,8 @@
 using System.IO;
 using System.Threading;
+using System.Text;
+using Unity.Jobs;
+using Unity.Collections;
 
 namespace CIC.Data
 {
@@ -16,15 +19,19 @@ namespace CIC.Data
         }
         public static void SaveDataThreat(string fullPath, byte[] data)
         {
-            SaveJob job = new SaveJob(fullPath, data);
-            Thread thread = new Thread(new ThreadStart(job.ExcuteBytes));
-            thread.Start();
+            //SaveJob job = new SaveJob(fullPath, data);
+            //Thread thread = new Thread(new ThreadStart(job.ExcuteBytes));
+            //thread.Start();
+            WriteByteJob job = new WriteByteJob(fullPath, data);
+
         }
         public static void SaveDataThreat(string fullPath, string data)
         {
-            SaveJob job = new SaveJob(fullPath, data);
-            Thread thread = new Thread(new ThreadStart(job.ExcuteText));
-            thread.Start();
+            //SaveJob job = new SaveJob(fullPath, data);
+            //Thread thread = new Thread(new ThreadStart(job.ExcuteText));
+            //thread.Start();
+
+            WriteTextJob job = new WriteTextJob(fullPath, data);
         }
 
 
@@ -55,5 +62,83 @@ namespace CIC.Data
         {
             File.WriteAllText(fullPath, textData);
         }
+    }
+
+    public class WriteByteJob
+    {
+        public string fullPath;
+        public byte[] data;
+
+        public WriteByteJob(string fullPath, byte[] data)
+        {
+            this.fullPath = fullPath;
+            this.data = data;
+            Execute();
+        }
+
+        private void Execute()
+        {
+            NativeArray<byte> nativeData = new NativeArray<byte>(data, Allocator.TempJob);
+            NativeArray<char> nativePath = new NativeArray<char>(fullPath.ToCharArray(), Allocator.TempJob);
+
+            WriteJobs jobs = new WriteJobs() { fileData = nativeData, fileFullPath = nativePath };
+
+            JobHandle jobHandle = jobs.Schedule();
+
+            jobHandle.Complete();
+
+            nativeData.Dispose();
+            nativePath.Dispose();
+        }
+        private struct WriteJobs : IJob
+        {
+            public NativeArray<byte> fileData;
+            public NativeArray<char> fileFullPath;
+
+            public void Execute()
+            {
+                File.WriteAllBytes(new string(fileFullPath.ToArray()), fileData.ToArray());
+            }
+        }
+
+    }
+
+    public class WriteTextJob
+    {
+        public string fullPath;
+        public string data;
+
+        public WriteTextJob(string fullPath, string data)
+        {
+            this.fullPath = fullPath;
+            this.data = data;
+            Execute();
+        }
+
+        private void Execute()
+        {
+            NativeArray<char> nativeData = new NativeArray<char>(data.ToCharArray(), Allocator.TempJob);
+            NativeArray<char> nativePath = new NativeArray<char>(fullPath.ToCharArray(), Allocator.TempJob);
+
+            WriteJobs jobs = new WriteJobs() { fileData = nativeData, fileFullPath = nativePath };
+
+            JobHandle jobHandle = jobs.Schedule();
+
+            jobHandle.Complete();
+
+            nativeData.Dispose();
+            nativePath.Dispose();
+        }
+        private struct WriteJobs : IJob
+        {
+            public NativeArray<char> fileData;
+            public NativeArray<char> fileFullPath;
+
+            public void Execute()
+            {
+                File.WriteAllText(new string(fileFullPath.ToArray()), new string(fileData.ToArray()));
+            }
+        }
+
     }
 }
